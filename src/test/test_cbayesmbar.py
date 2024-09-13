ac5bee76f11984666345c30b9cb2c8d7f89c6698
Xinqiang Ding <Xinqiang.Ding@tufts.edu>
2024-09-13 12:38:05 -0400

    use mkdocs for doc

--- src/test/test_cbayesmbar.py
+++ src/test/test_cbayesmbar.py
@@ -6,7 +6,7 @@ import numpy as np
 
 @pytest.fixture
 def setup_data_cbmbar():
-    ka, kb, kc, kd = 9, 16, 25, 36
+    ka, kb, kc, kd = 4, 9, 16, 25
     ## equilibrium positions of three two-dimensional harmonic oscillators
     mu = {
         "a": np.array([-1.0, 1.0]),
@@ -30,12 +30,11 @@ def setup_data_cbmbar():
             mu[(s1, s2, idx)] = mu[s1] + (mu[s2] - mu[s1]) / 4 * idx
             k[(s1, s2, idx)] = k[s1] + (k[s2] - k[s1]) / 4 * idx
 
+    n = 2000
 
-    n = 1000
     ## compute the free energy difference between pairs of states analytically
     def k_to_sigma(k):
         return np.sqrt(1.0 / k)
-
 
     deltaF_ref = {}
     for s1, s2 in pairs:
@@ -43,8 +42,8 @@ def setup_data_cbmbar():
             -np.log(k_to_sigma(k[s2])).sum() + np.log(k_to_sigma(k[s1])).sum()
         )
 
-
     ## sample configurations from all states including both endpoints and intermediates
+    np.random.seed(0)
     x = {}
     for s in mu.keys():
         x[s] = np.random.normal(mu[s], k_to_sigma(k[s]), (n, 2))
@@ -53,13 +52,14 @@ def setup_data_cbmbar():
     for s1, s2 in pairs:
         key = [s1] + [(s1, s2, idx) for idx in range(1, 4)] + [s2]
         xs = np.concatenate([x[s] for s in key])
-
-        u = np.stack([np.sum(0.5 * k[s] * (xs - mu[s]) ** 2, axis=1) for s in key], axis=0)
-        num_conf = np.array([u.shape[1] // u.shape[0] for _ in range(u.shape[0])])
-
+        u = np.stack(
+            [np.sum(0.5 * k[s] * (xs - mu[s]) ** 2, axis=1) for s in key], axis=0
+        )
         u_list.append(u)
 
-    num_conf_list = [[u.shape[1] // u.shape[0] for i in range(u.shape[0])] for u in u_list]
+    num_conf_list = [
+        [u.shape[1] // u.shape[0] for i in range(u.shape[0])] for u in u_list
+    ]
     identical_states = (
         [(0, 0), (3, 4), (4, 0)],
         [(0, 4), (1, 0), (5, 0)],
@@ -70,9 +70,12 @@ def setup_data_cbmbar():
     return u_list, num_conf_list, identical_states, pairs, deltaF_ref
 
 
-def test_CBayesMBAR(setup_data_cbmbar):
+@pytest.mark.parametrize("method", ["Newton", "L-BFGS-B"])
+def test_CBayesMBAR(setup_data_cbmbar, method):
     u_list, num_conf_list, identical_states, pairs, deltaF_ref = setup_data_cbmbar
-    cbmbar = CBayesMBAR(u_list, num_conf_list, identical_states, random_seed=0)
+    cbmbar = CBayesMBAR(
+        u_list, num_conf_list, identical_states, method=method, random_seed=0
+    )
 
     deltaF_cbmbar = {}
     for i in range(len(pairs)):
